2ac82b6a43470625226d5c125c703ec8cba21444
gomachan46 <shiro.gomachan46@gmail.com>
2016-02-29 02:28:36 +0900

    テストは今回のexampleでは捨てる

--- test/test.js
+++ test/test.js
@@ -22,20 +22,4 @@ describe("App", () => {
     it("outputとしてdivが返ってくること", () => {
         assert(output.type === 'div')
     })
-
-    describe("div内", () => {
-        const h1 = output.props.children
-
-        it("要素としてh1が返ってくること", () => {
-            assert(h1.type === 'h1')
-        })
-
-        describe("h1内", () => {
-            const h1Content = h1.props.children
-
-            it("要素としてh1が返ってくること", () => {
-                assert(h1Content === 'Test')
-            })
-        })
-    })
 })
